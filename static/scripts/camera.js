c7b9555d21ab77c996d21533d0a9651ca535bc22
Aleksei Stukalov <stukalov.nz@gmail.com>
2023-07-03 17:46:19 +1200

    openCV.js added

---
+++ static/scripts/camera.js
@@ -0,0 +1,51 @@
+const camera = document.querySelector('#camera')
+const shutter = document.querySelector('#shutter')
+const canvas = document.querySelector('#canvas')
+const preview = document.querySelector('#preview')
+
+let src = new cv.Mat(camera.height, camera.width, cv.CV_8UC4)
+let dst = new cv.Mat(camera.height, camera.width, cv.CV_8UC4)
+let gray = new cv.Mat()
+let cap = new cv.VideoCapture(camera)
+let faces = new cv.RectVector()
+let classifier = new cv.CascadeClassifier()
+
+classifier.load('haarcascade_frontalface_default.xml')
+
+const FPS = 30
+function processVideo() {
+  try {
+    if (!streaming) {
+      // clean and stop.
+      src.delete()
+      dst.delete()
+      gray.delete()
+      faces.delete()
+      classifier.delete()
+      return
+    }
+    let begin = Date.now()
+    // start processing.
+    cap.read(src)
+    src.copyTo(dst)
+    cv.cvtColor(dst, gray, cv.COLOR_RGBA2GRAY, 0)
+    // detect faces.
+    classifier.detectMultiScale(gray, faces, 1.1, 3, 0)
+    // draw faces.
+    for (let i = 0; i < faces.size(); ++i) {
+      let face = faces.get(i)
+      let point1 = new cv.Point(face.x, face.y)
+      let point2 = new cv.Point(face.x + face.width, face.y + face.height)
+      cv.rectangle(dst, point1, point2, [255, 0, 0, 255])
+    }
+    cv.imshow('canvasOutput', dst)
+    // schedule the next one.
+    let delay = 1000 / FPS - (Date.now() - begin)
+    setTimeout(processVideo, delay)
+  } catch (err) {
+    utils.printError(err)
+  }
+};
+
+// schedule the first one.
+setTimeout(processVideo, 0)
